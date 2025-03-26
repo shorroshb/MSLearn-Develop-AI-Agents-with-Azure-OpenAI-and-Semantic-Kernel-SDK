2a7c3708758e79e40b90980d260cc9090475f963
shorroshb <125075425+shorroshb@users.noreply.github.com>
2025-03-26 10:53:56 -0600

    Update Program.cs

--- M02-create-plugins-for-semantic-kernel/M02-Project/Program.cs
+++ M02-create-plugins-for-semantic-kernel/M02-Project/Program.cs
@@ -36,3 +36,49 @@ var arguments = new KernelArguments { ["city"] = city, ["background"] = backgrou
 // InvokeAsync on the kernel object
 var result = await kernel.InvokeAsync(activitiesFunction, arguments);
 Console.WriteLine(result);
+
+/*
+// Create a kernel with Azure OpenAI chat completion
+var builder = Kernel.CreateBuilder();
+builder.AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
+
+// Build the kernel
+Kernel kernel = builder.Build();
+
+string prompt = """
+    <message role="system">Instructions: Identify the from and to destinations 
+    and dates from the user's request</message>
+
+    <message role="user">Can you give me a list of flights from Seattle to Tokyo? 
+    I want to travel from March 11 to March 18.</message>
+
+    <message role="assistant">
+    Origin: Seattle
+    Destination: Tokyo
+    Depart: 03/11/2025 
+    Return: 03/18/2025
+    </message>
+
+    <message role="user">{{input}}</message>
+    """;
+    ```
+
+string input = "I want to travel from June 1 to July 22. I want to go to Greece. I live in Chicago.";
+
+// Create the kernel arguments
+var arguments = new KernelArguments { ["input"] = input };
+
+// Create the prompt template config using handlebars format
+var templateFactory = new HandlebarsPromptTemplateFactory();
+var promptTemplateConfig = new PromptTemplateConfig()
+{
+    Template = prompt,
+    TemplateFormat = "handlebars",
+    Name = "FlightPrompt",
+};
+
+// Invoke the prompt function
+var function = kernel.CreateFunctionFromPrompt(promptTemplateConfig, templateFactory);
+var response = await kernel.InvokeAsync(function, arguments);
+Console.WriteLine(response);
+*/
